cafeabd4f4cde85ee99597f50dd56c700bc1ccd9
Shamoon Siddiqui <shamoon@develop.io>
2019-03-27 08:45:07 -0400

    CIFAR

--- whales.py
+++ whales.py
@@ -1,14 +1,48 @@
-import autokeras as ak
-from autokeras.image.image_supervised import load_image_dataset
+import keras
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Flatten
 
-x_train, y_train = load_image_dataset(csv_file_path="data/train.1.csv",
-                                      images_path="data/train")
 
-# print(x_train.shape)
-# print(y_train.shape)
-x_train = x_train.reshape(x_train.shape + (1,))
-# y_train = y_train.reshape(y_train.shape + (1,))
+num_classes = 10
+batch_size = 100
+epochs = 100
 
-clf = ak.ImageClassifier(verbose=True, augment=False)
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test),
+          shuffle=True,
+          verbose=1)
